0c11c2108f67dbcdc23e8649e8ce969071e4bf95
Sam <huy.le09@protonmail.com>
2022-12-16 16:42:54 -0800

    Add script to unmount all game service for development

---
+++ unmountall.sh
@@ -0,0 +1,3 @@
+sudo umount /home/student/Documents/cs449/final-wordle/var/primary/mount
+sudo umount /home/student/Documents/cs449/final-wordle/var/secondary/mount
+sudo umount /home/student/Documents/cs449/final-wordle/var/tertiary/mount
